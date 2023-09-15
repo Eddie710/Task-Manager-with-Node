b83f3c74773a0863c5a6fcba022e5f83cf36f17d
Eddie710 <111469943+Eddie710@users.noreply.github.com>
2023-09-15 14:20:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.get('/delete/:id', (req, res) => {
     res.redirect('/');
   });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
 });
